9ef8ee8755eb6d87e888bb9c57f46c63c7ae5f91
aGonzalezHdez <agonzalez.mex@grupoei.com.mx>
2026-01-14 12:45:43 -0600

    fix: Se elimina datos de bd explicitos en codigo local

--- Presentation_Console/Program.cs
+++ Presentation_Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Registration;
 using FluentValidation;
 using Infrastructure.Registration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -18,8 +19,20 @@ class Program
                 rollingInterval: RollingInterval.Day)
             .CreateLogger();
         using var host = Host.CreateDefaultBuilder(args).UseSerilog()
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddUserSecrets<Program>(optional: true);
+            })
             .ConfigureServices((context, services) =>
             {
+                var cs = context.Configuration.GetValue<string>("Sql:ConnectionString");
+                if (string.IsNullOrWhiteSpace(cs))
+                {
+                    throw new InvalidOperationException(
+                        "No se encontró 'Sql:ConnectionString'. " +
+                        "Verifica que DOTNET_ENVIRONMENT=Development y que los user-secrets estén en el csproj de Presentation_Console.");
+                }
+
                 // Registramos capas
                 services.ApplicationServices(context.Configuration);
                 services.InfrastructureServices(context.Configuration);
